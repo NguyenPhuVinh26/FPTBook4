05e8c1facb32f4c4ea8ca3393229ba623bd38087
NguyenPhuVinh26 <91648114+NguyenPhuVinh26@users.noreply.github.com>
2022-03-20 09:04:22 +0700

    Create function add, update, and delete publisher

---
+++ WebSiteBanSach4/WebSiteBanSach4/Controllers/NhaXuatBanController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSiteBanSach4.Models;
+namespace WebSiteBanSach4.Controllers
+{
+    public class NhaXuatBanController : Controller
+    {
+        // GET: NhaXuatBan
+        QuanLyBanSachEntities db = new QuanLyBanSachEntities();
+
+        public PartialViewResult NhaXuatBanPartial()
+        {
+            return PartialView(db.NhaXuatBans.Take(10).OrderBy(x => x.TenNXB).ToList());
+        }
+        public ViewResult SachTheoNXB(int MaNXB)
+        { 
+            NhaXuatBan nxb = db.NhaXuatBans.SingleOrDefault(n => n.MaNXB == MaNXB);
+            if(nxb==null)
+            {
+                Response.StatusCode=404;
+                return null;
+            }
+            List<Sach> lstsach = db.Saches.Where(n=>n.MaNXB==MaNXB).OrderBy(n=>n.GiaBan).ToList();
+            if(lstsach.Count==0)
+            {
+                ViewBag.Sach = "There are no books on this topic";
+            }
+            return View(lstsach);
+        }
+        public ViewResult DanhMucNXB()
+        {
+            return View(db.NhaXuatBans.ToList());
+        }
+     }
+}
---
+++ WebSiteBanSach4/WebSiteBanSach4/Controllers/QuanLyNXBController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSiteBanSach4.Models;
+using PagedList.Mvc;
+using PagedList;
+
+namespace WebSiteBanSach4.Controllers
+{
+    public class QuanLyNXBController : Controller
+    {
+        QuanLyBanSachEntities db = new QuanLyBanSachEntities();
+        // GET: QuanLyNXB
+        public ActionResult Index(int? page)
+        {
+            int pageNumber = (page ?? 1);
+            int pageSize = 10;
+            return View(db.NhaXuatBans.ToList().OrderBy(n => n.TenNXB).ToPagedList(pageNumber, pageSize));
+        }
+        [HttpGet]
+        public ActionResult ThemMoi()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult ThemMoi(NhaXuatBan nhaxuatban)
+        {
+            if (ModelState.IsValid)
+            {
+                db.NhaXuatBans.Add(nhaxuatban);
+                db.SaveChanges();
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ChinhSua(int MaNXB)
+        {
+            NhaXuatBan nhaxuatban = db.NhaXuatBans.SingleOrDefault(n => n.MaNXB == MaNXB);
+            if (nhaxuatban == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(nhaxuatban);
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult ChinhSua(NhaXuatBan nhaxuatban)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(nhaxuatban).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+            return View();
+        }
+
+        public ActionResult HienThi(int MaNXB)
+        {
+            NhaXuatBan nhaxuatban = db.NhaXuatBans.SingleOrDefault(n => n.MaNXB == MaNXB);
+            if (nhaxuatban == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(nhaxuatban);
+        }
+
+        [HttpGet]
+        public ActionResult Xoa(int MaNXB)
+        {
+            NhaXuatBan nhaxuatban = db.NhaXuatBans.SingleOrDefault(n => n.MaNXB == MaNXB);
+            if (nhaxuatban == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(nhaxuatban);
+        }
+
+        [HttpPost, ActionName("Xoa")]
+        public ActionResult XacNhanXoa(int MaNXB)
+        {
+            NhaXuatBan nhaxuatban = db.NhaXuatBans.SingleOrDefault(n => n.MaNXB == MaNXB);
+            if (nhaxuatban == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            db.NhaXuatBans.Remove(nhaxuatban);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}

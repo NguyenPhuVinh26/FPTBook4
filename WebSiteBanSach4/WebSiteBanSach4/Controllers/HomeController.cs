d8b399ffbf8bef5d3be07ae6d9817f8cf8e36bdb
NguyenPhuVinh26 <91648114+NguyenPhuVinh26@users.noreply.github.com>
2022-03-21 23:59:43 +0700

    Add product, publisher, author, category

--- WebSiteBanSach4/WebSiteBanSach4/Controllers/HomeController.cs
+++ WebSiteBanSach4/WebSiteBanSach4/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace WebSiteBanSach4.Controllers
         QuanLyBanSachEntities db = new QuanLyBanSachEntities();
         public ActionResult Index(int? page)
         {
-            int pageSize = 9;
+            int pageSize = 10;
             int pageNumber = (page ?? 1);
             return View(db.Saches.Where(n=>n.Moi==1).OrderBy(n=>n.GiaBan).ToPagedList(pageNumber,pageSize));
         }
